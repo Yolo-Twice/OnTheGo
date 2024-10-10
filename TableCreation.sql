e37df91a5196dfeabe23a22fb79ded7746f30dfd
Yolo-Twice <88250652+Yolo-Twice@users.noreply.github.com>
2024-10-10 19:47:34 +0530

    Product table added
    Added the product table

--- TableCreation.sql
+++ TableCreation.sql
@@ -1,1 +1,2 @@
 create database OnTheGo
+create table Product(CustID int PRIMARY KEY, CustName VARCHAR(25),CustEmail VARCHAR(25), CustPhone int, IsMember BOOLEAN, Points int); --Creates the product table
