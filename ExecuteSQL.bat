ac59622b54557712abc14d877221db067c8caf38
Yolo-Twice <88250652+Yolo-Twice@users.noreply.github.com>
2024-10-15 22:59:39 +0530

    Create ExecuteSQL.bat

---
+++ ExecuteSQL.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+:: Prompt for MySQL username
+set /p MYSQL_USER="Enter MySQL username: "
+
+:: Prompt for MySQL password
+set /p MYSQL_PASSWORD="Enter MySQL password: "
+
+:: Define your SQL files
+set SQL_FILE1=".\TableCreation.sql"
+set SQL_FILE2=".\dataInsertion.sql"
+set SQL_FILE3=".\Triggers.sql"
+
+:: Execute the SQL files
+mysql -u %MYSQL_USER% -p%MYSQL_PASSWORD% -e "source %SQL_FILE1%"
+mysql -u %MYSQL_USER% -p%MYSQL_PASSWORD% -e "source %SQL_FILE2%"
+mysql -u %MYSQL_USER% -p%MYSQL_PASSWORD% -e "source %SQL_FILE3%"
+
+echo SQL files executed successfully.
+
+endlocal
+pause
